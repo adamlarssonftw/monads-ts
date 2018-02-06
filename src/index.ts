a0aa3571f4a346b8073b1e37f02829ab27f658b4
Desktop <adamlarsson92@gmail.com>
2018-02-06 23:47:50 +0100

    refactor(index): clean up entry file a bit.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import {pipe} from './pipe';
 
 const f = (x) => x * 2;
-const g = (x) => x/2;
+const g = (x) => x / 2;
 const h = (x) => Math.pow(x, 4);
 
-const y = pipe(2, [f, h, g]);
-console.log(y);
+console.log('pipe outputs', pipe(2, [f, h, g]));
