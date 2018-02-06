815bc783f99f1d1d8372ebd31cac16d2a9af762f
Desktop <adamlarsson92@gmail.com>
2018-02-06 23:49:58 +0100

    refactor(index): try functor out in entry file.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import {pipe} from './pipe';
+import {Functor} from './functor';
 
 const f = (x) => x * 2;
 const g = (x) => x / 2;
 const h = (x) => Math.pow(x, 4);
 
 console.log('pipe outputs', pipe(2, [f, h, g]));
+console.log('functor maps to', new Functor(2).map(f));
