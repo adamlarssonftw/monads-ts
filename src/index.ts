888dc0be65e733d533b13afbba78739678237950
Desktop <adamlarsson92@gmail.com>
2018-02-27 22:21:25 +0100

    fix(utilities): update references to functions.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import {pipe} from './utils/pipe';
 import {Functor} from './functor';
 import {Monad} from "./monads/monad";
 import {MaybeMonad} from "./monads/maybe-monad";
+import {UtilityFunctions} from "./utils/utility-functions";
 
 const f = (x) => x * 2;
 const g = (x) => x / 2;
 const h = (x) => Math.pow(x, 4);
 const length = (x) => x.length;
 
-//TODO: naive replace assertions with proper unit tests.
-console.log('pipe outputs', pipe(2, [f, h, g]));
+console.log('pipe outputs', UtilityFunctions.pipe(2, [f, h, g]));
 console.log('functor maps to', new Functor(2).fmap(f));
 console.log('functor null', new Functor(null).fmap(f));
 console.log('monad maps to', new Monad<string>('hello').fmap(length));
